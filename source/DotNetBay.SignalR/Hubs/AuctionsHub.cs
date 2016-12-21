e76aeb15ad2702711dde407c70f5963060b6e017
Matthias Frey <matthias.frey@students.fhnw.ch>
2016-12-21 14:16:58 +0100

    signalR lab

---
+++ source/DotNetBay.SignalR/Hubs/AuctionsHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetBay.Data.Entity;
+using Microsoft.AspNet.SignalR;
+
+namespace DotNetBay.SignalR.Hubs
+{
+    public class AuctionsHub : Hub
+    {
+        public static void NotifyNewAuction(Auction auction) { }
+
+        public static void NotifyBidAccepted(Auction auction, Bid bid) { }
+
+        public static void NotifyAuctionStarted(Auction auction) { }
+
+        public static void NotifyAuctionEnded(Auction auction) { }
+    }
+}
---
+++ source/DotNetBay.WebApp/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using DotNetBay.SignalR.Hubs;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DotNetBay.WebApp.Startup))]
+
+namespace DotNetBay.WebApp
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {          // Configure Web API for self-host.           
+            AppDomain.CurrentDomain.Load(typeof(AuctionsHub).Assembly.FullName);
+            app.MapSignalR();
+
+        }
+    }
+}
